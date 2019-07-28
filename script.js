89a79d055022aa85ae8e6df1756464c252285edc
Natalie-Cardot <nataliecardot@gmail.com>
2019-07-27 20:08:45 -0700

    Fix scroll location offset for chevron link

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 
 let sectionId;
 
-$('.nav-link').click((e) => {
-  sectionId = $(e.target).attr('href');
+// .attr returns value of attribute
+// e.currentTarget needed for this to work with chevron icon since it's inside an a element, whereas e.Target worked fine with .nav-link. This is because currentTarget refers to the element the event listener is directly attached to--in this case the a element, whereas with e.target, it would be the i element that triggered the event/on which event originally occurred (before it bubbled up), which doesn't have an href element (so attr would return undefined)
+$('.nav-link, .chevron').click((e) => {
+  sectionId = $(e.currentTarget).attr('href');
   $('html, body').animate({
     scrollTop: $(sectionId).offset().top - 20
   }, 100);
