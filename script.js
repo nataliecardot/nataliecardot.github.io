8da49dce7f6939f25ece7e194f484f2e13c7f1cd
Natalie-Cardot <nataliecardot@gmail.com>
2020-12-06 23:31:18 -0800

    Remove picture

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let sectionId;
 // .attr returns value of attribute
 // e.currentTarget needed for this to work with chevron icon since it's inside an a element, whereas e.Target worked fine with .nav-link. This is because currentTarget refers to the element the event listener is directly attached to--in this case the a element, whereas with e.target, it would be the i element that triggered the event/on which event originally occurred (before it bubbled up), which doesn't have an href element (so attr would return undefined)
 
-$(function() {
-  $(window).on('resize', function(e) {
+$(function () {
+  $(window).on('resize', function (e) {
     checkScreenSize();
   });
 
@@ -15,11 +15,11 @@ $(function() {
   function checkScreenSize() {
     var newWindowWidth = $(window).width();
     if (newWindowWidth < 480) {
-      $('.nav-link, .chevron').click(e => {
+      $('.nav-link, .chevron').click((e) => {
         sectionId = $(e.currentTarget).attr('href');
         $('html, body').animate(
           {
-            scrollTop: $(sectionId).offset().top - 30
+            scrollTop: $(sectionId).offset().top - 30,
           },
           100
         );
@@ -28,19 +28,19 @@ $(function() {
   }
 });
 
-$(window).resize(function() {
-  $('.nav-link, .chevron').click(e => {
+$(window).resize(function () {
+  $('.nav-link, .chevron').click((e) => {
     sectionId = $(e.currentTarget).attr('href');
     $('html, body').animate(
       {
-        scrollTop: $(sectionId).offset().top - 30
+        scrollTop: $(sectionId).offset().top - 30,
       },
       100
     );
   });
 });
 
-$(document).on('scroll', function() {
+$(document).on('scroll', function () {
   const wHeight = window.innerHeight;
   // As of 2019 scrollY not supported by IE, but pageYOffset is supported by all browsers
   const yScroll = window.scrollY || window.pageYOffset;
